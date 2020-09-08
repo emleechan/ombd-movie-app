4181b0a446ab766a860595f763efe245bdb7eabc
Emily Chan <emc15@sfu.ca>
2020-09-07 22:48:16 -0700

    Updates

---
+++ precache-manifest.994aced21e7c04206ed671f81bbc1d50.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "275f876c40c91c6c2f628a2889ea88e4",
+    "url": "/ombd-movie-app/index.html"
+  },
+  {
+    "revision": "d65f63d670fb75851f77",
+    "url": "/ombd-movie-app/static/css/2.48b2f9f6.chunk.css"
+  },
+  {
+    "revision": "0176f8c6c8a777534de9",
+    "url": "/ombd-movie-app/static/css/main.16adb2a4.chunk.css"
+  },
+  {
+    "revision": "d65f63d670fb75851f77",
+    "url": "/ombd-movie-app/static/js/2.c8aedf54.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/ombd-movie-app/static/js/2.c8aedf54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0176f8c6c8a777534de9",
+    "url": "/ombd-movie-app/static/js/main.e2b9f977.chunk.js"
+  },
+  {
+    "revision": "f281601b863c3888632b",
+    "url": "/ombd-movie-app/static/js/runtime-main.355756f3.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/ombd-movie-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/ombd-movie-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/ombd-movie-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/ombd-movie-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/ombd-movie-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/ombd-movie-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/ombd-movie-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/ombd-movie-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/ombd-movie-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/ombd-movie-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/ombd-movie-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/ombd-movie-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/ombd-movie-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/ombd-movie-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/ombd-movie-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/ombd-movie-app/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
